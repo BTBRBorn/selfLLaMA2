73a8e0fc93c57289a7a7f6d3fa57900dafe0379e
Onur Yesil <onuryesil@gmail.com>
2025-03-29 00:50:19 +0300

    Update model_inference.py, README.md - model_inference.py is finished.

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+This repository is a from scratch implementation of LLaMA2-3 models.
+It is still under development but when it is done, it will include inference and training
+code together.
+For now, model_inference.py is implemented which can be used to create a model for inference.
+You can see the implementations of RoPE(Rotary Positional Encodings), KV cache,
+GQA(Grouped Query Attention), SwiGLU activation which are essential components of modern LLMs.
--- model_inference.py
+++ model_inference.py
@@ -1,21 +1,23 @@
 import torch
 from torch import nn
 from dataclasses import dataclass
+import torch.nn.functional as F
 
 
 @dataclass
 class ModelArgs:
-    dim: int = 8
+    dim: int = 128 
+    vocab_size: int = 50234 
     rms_eps: float = 1e-6
-    n_layers: int = 6
-    n_heads: int = 16
-    n_kv_heads: int | None = 4
+    n_layers: int = 8
+    n_heads: int = 4
+    n_kv_heads: int | None = 2
 
     # hyperparameters related to kv_cache
-    max_batch_size: int = 32
-    max_seq_len: int = 8
+    max_batch_size: int = 4
+    max_seq_len: int = 256
 
-    device: str = "cpu"
+    device: str = "cuda"
 
 
 def pre_compute_cis(head_dim: int, seq_len: int, theta_base: float = 1e4):
@@ -54,14 +56,16 @@ class RMSNorm(nn.Module):
         return (x * self.scalar) * self.rec_rms(x)
 
 
+# Self attention with KV cache and GQA
 class SelfAttention(nn.Module):
     def __init__(self, args: ModelArgs) -> None:
+        super().__init__()
         self.n_heads = args.n_heads
         self.head_dim = args.dim // args.n_heads
         self.n_kv_heads = (
             args.n_kv_heads if args.n_kv_heads is not None else args.n_heads
         )
-        self.n_repeat = args.n_heads // self.n_kv_heads
+        self.n_repeats = args.n_heads // self.n_kv_heads
 
         self.wq = nn.Linear(args.dim, args.dim, bias=False)
         self.wk = nn.Linear(args.dim, self.head_dim * self.n_kv_heads, bias=False)
@@ -70,10 +74,10 @@ class SelfAttention(nn.Module):
 
         self.k_cache = torch.zeros(
             size=(args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim)
-        )
+        ).to(args.device)
         self.v_cache = torch.zeros(
             size=(args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim)
-        )
+        ).to(args.device)
 
     def forward(
         self, x: torch.Tensor, start_pos: int, theta_cis: torch.Tensor
@@ -82,9 +86,9 @@ class SelfAttention(nn.Module):
         # seq_len always equals to 1
         # (batch_size, seq_len, dim) -> (batch_size, seq_len, n_heads, head_dim)
         xq = self.wq(x).view(batch_size, seq_len, self.n_heads, self.head_dim)
-        # (batch_size, seq_len, dim) -> (batch_size, seq_len, self.n_kv_head, self.head_dim)
+        # (batch_size, seq_len, dim) -> (batch_size, seq_len, n_kv_head, self.head_dim)
         xk = self.wk(x).view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
-        # (batch_size, seq_len, dim) -> (batch_size, seq_len, self.n_kv_head, self.head_dim)
+        # (batch_size, seq_len, dim) -> (batch_size, seq_len, n_kv_head, self.head_dim)
         xv = self.wv(x).view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
 
         # (batch_size, seq_len, n_kv_head, head_dim) -> (batch_size, seq_len, n_kv_head, head_dim)
@@ -92,13 +96,52 @@ class SelfAttention(nn.Module):
         xv = apply_rotary_pos_embeddings(xv, theta_cis)
 
         # Cache the current k, v vectors
-        self.k_cache[:batch_size, start_pos : start_pos + 1] = xk
-        self.v_cache[:batch_size, start_pos : start_pos + 1] = xv
+        self.k_cache[:batch_size, start_pos : start_pos + seq_len] = xk
+        self.v_cache[:batch_size, start_pos : start_pos + seq_len] = xv
 
         # Get all the k and v's from cache
         # (batch_size, start_pos+1, n_kv_head, head_dim) -> (batch_size, start_pos+1, n_kv_head, head_dim)
-        xk = self.k_cache[:batch_size, : start_pos + 1]
-        xv = self.v_cache[:batch_size, : start_pos + 1]
+        xk = self.k_cache[:batch_size, : start_pos + seq_len]
+        xv = self.v_cache[:batch_size, : start_pos + seq_len]
+
+        # Copy key and value heads to match query
+        # (batch_size, start_pos+1, n_kv_head, head_dim) -> (batch_size, start_pos+1, n_heads, head_dim)
+        xk = xk.repeat_interleave(repeats=self.n_repeats, dim=-2)
+        xv = xv.repeat_interleave(repeats=self.n_repeats, dim=-2)
+
+        # Transpose to make n_head to be part of batch dimensions
+        # (batch_size, seq_len, n_heads, head_dim) -> (batch_size, n_heads, seq_len, head_dim)
+        xq = xq.transpose(1, 2)
+        # (batch_size, start_pos+1, n_heads, head_dim) -> (batch_size, n_heads, start_pos+1, head_dim)
+        xk = xk.transpose(1, 2)
+        xv = xv.transpose(1, 2)
+
+        # Remember seq_len = 1
+        # (batch_size, n_head, 1, head_dim) @ (batch_size, n_heads, head_dim, start_pos+1)
+        # -> (batch_size, n_heads, 1, start_pos+1)
+        att = (xq @ xk.transpose(-1, -2)) * (self.head_dim**-0.5)
+        # (batch_size, n_heads, 1, start_pos+1) @ (batch_size, n_heads, start_pos+1, head_dim)
+        # -> (batch_size, n_heads, 1, head_dim)
+        y = F.softmax(att @ xv, dim=-1)
+
+        # (batch_size, n_heads, 1, head_dim) -> (batch_size, 1, dim)
+        y = y.transpose(1, 2).view(batch_size, seq_len, self.n_heads * self.head_dim)
+
+        return self.wo(y)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.W1 = nn.Linear(args.dim, args.dim * 4, bias=False)
+        self.W2 = nn.Linear(args.dim, args.dim * 4, bias=False)
+        self.W3 = nn.Linear(args.dim * 4, args.dim, bias=False)
+        self.silu = nn.SiLU()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        gate = self.silu(self.W1(x))
+        x = gate * self.W2(x)
+        return self.W3(x)
 
 
 class Block(nn.Module):
@@ -130,15 +173,15 @@ class Transformer(nn.Module):
         self.ln_rms = RMSNorm(args.dim, eps=args.rms_eps)
         self.ln_head = nn.Linear(args.dim, args.vocab_size, bias=False)
 
-        # Tie the embeddings and lm head heads
-        self.ln_head.weight = self.tok_emb.weight
-
     def forward(self, x: torch.Tensor, start_pos: int) -> torch.Tensor:
         # (batch_size, seq_len) -> (batch_size, seq_len, dim)
         batch_size, seq_len = x.shape
+        assert seq_len == 1, (
+            f"Inference code is using kv cache therefore sequence length has to be 1 not {seq_len}"
+        )
         x = self.tok_emb(x)
         theta_cis = self.theta_cis[start_pos : start_pos + seq_len]
         for block in self.blocks:
-            x = block(x, theta_cis, start_pos)
+            x = block(x, start_pos, theta_cis)
         x = self.ln_rms(x)
         return self.ln_head(x)
